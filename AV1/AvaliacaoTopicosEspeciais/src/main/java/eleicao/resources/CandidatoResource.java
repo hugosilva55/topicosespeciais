45aefcaf9abdcfaa0ac30037cd698f60009b5c17
unknown <hugosilva05512@gmail.com>
2018-09-25 19:55:22 -0300

    av1

--- AV1/AvaliacaoTopicosEspeciais/src/main/java/eleicao/resources/CandidatoResource.java
+++ AV1/AvaliacaoTopicosEspeciais/src/main/java/eleicao/resources/CandidatoResource.java
@@ -1,7 +1,11 @@
 package eleicao.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import eleicao.domain.Candidato;
 import eleicao.services.CandidatoService;
